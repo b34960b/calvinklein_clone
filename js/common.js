e4b4f92204a3c7653b53ab39e12d364f89986cc3
b34960b <b34960b@gmail.com>
2020-08-23 19:20:06 +0900

    calvinklein

--- js/common.js
+++ js/common.js
@@ -6,6 +6,9 @@
   const container = $('#container');
   const footBox = $('#footBox');
 
+  const headFoot = $('.header-foot');
+  const headNoneScroll = headFoot.offset().top;
+
   win.on('scroll', function(){
     let winScroll = win.scrollTop();
     // console.log(winScroll);
@@ -15,8 +18,17 @@
       headBox.removeClass('action');
       // console.log('class remove')
     }
+
+    if (winScroll >= headNoneScroll - 70) {
+      $('.display-None').css("display", "block");
+      headBox.css("display", "none");
+    } else {
+      $('.display-None').css("display", "none");
+      headBox.css("display", "block");
+    } //헤더 복사 후, 기존 헤더 숨기고 새로운 헤더는 포지션 기본으로 스르륵 올라가기
   }); //슬라이드배너 스크롤 내릴때 사라지기
 
+  
   
   const slide = $('.prompSlide');
   let i = 0;
